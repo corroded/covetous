9681e7dc5e0be3440ff22c4cd48acf0ac07c0513
Eumir Gaspar <eumir@aelogica.com>
2012-09-17 15:34:18 +0800

    made hero inherit from career profile

--- lib/covetous/profile/hero.rb
+++ lib/covetous/profile/hero.rb
@@ -1,20 +1,11 @@
 module Covetous
   module Profile
-    class Hero
+    class Hero < Covetous::Profile::Career
       attr_accessor :url, :response
 
       def initialize(battle_tag, hero_id)
         @url = "#{Covetous::Profile::BASE_URL}/#{battle_tag}/hero/#{hero_id}"
         @response = Covetous::Profile.get @url
-      end
-
-      def method_missing(name, *args, &block)
-        camelcased_key = name.to_s.camelize(:lower)
-        if @response.has_key?(camelcased_key)
-          @response[camelcased_key]
-        else
-          super
-        end
       end
     end
   end
