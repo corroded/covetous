7b0230a905de2db4c0e133af86ef5418b4fad301
Eumir Gaspar <eumir@aelogica.com>
2012-09-17 23:53:24 +0800

    renamed info -> data in specs

--- spec/covetous/data/item_spec.rb
+++ spec/covetous/data/item_spec.rb
@@ -15,6 +15,13 @@ describe 'Item' do
     it 'should have the correct url' do
       @my_item.url.must_equal 'http://us.battle.net/api/d3/data/item/CkMIz4LU4AoSBwgEFX35Tl0dLWYPvh194Mt2Ha3H5XEdElz0_B2H9XtuIgsIARWFQgMAGAAgCjAJOLYEQABIAVAOYPkEGPrl99QLUAZYAA'
     end
-  end
 
+    it 'should have the top level keys available as methods' do
+      top_level_keys = %w{ id name icon displayColor tooltipParams requiredLevel itemLevel bonusAffixes typeName type armor attributes attributesRaw socketEffects salvage gems }
+
+      top_level_keys.each do |tl_key|
+        @my_item.send(tl_key).must_equal @my_item.response[tl_key.camelize(:lower)]
+      end
+    end
+  end
 end
