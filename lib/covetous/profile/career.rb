c482b481b63cb93903b26ec0fa2c33b37c36f48b
Eumir Gaspar <eumir@aelogica.com>
2012-09-17 10:30:01 +0800

    second passing test. Also added method missing for keys in @info

--- lib/covetous/profile/career.rb
+++ lib/covetous/profile/career.rb
@@ -1,14 +1,24 @@
 module Covetous
   module Profile
     class Career
-      include HTTParty     
-
-      attr_accessor :url
+      attr_accessor :url, :info
 
       def initialize(battle_tag)
         @url = "#{Covetous::Profile::BASE_URL}/#{battle_tag}/"
+        @info = Covetous::Profile.get @url
       end
 
+      def hero_names
+        heroes.map{ |hero| hero['name'] }
+      end
+
+      def method_missing(name, *args, &block)
+        if @info.has_key?(name.to_s)
+          @info[name.to_s]
+        else
+          super
+        end
+      end
     end
   end
 end
--- spec/covetous/profile/career_spec.rb
+++ spec/covetous/profile/career_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
 
 describe 'Career' do
-  describe 'when given a battle tag name and id' do
-    before do
+  before do
+    # VCR.insert_cassette 'career_profile', :record => :once
+    VCR.use_cassette('career_profile') do
       @my_profile = Covetous::Profile::Career.new 'corroded-6950'
-      VCR.insert_cassette 'career_profile', :record => :once
     end
+  end
 
-    after do
-      VCR.eject_cassette
-    end
+  after do
+    VCR.eject_cassette
+  end
 
+  describe 'when given a battle tag name and id' do
     it 'should have the correct url' do
       @my_profile.url.must_equal 'http://us.battle.net/api/d3/profile/corroded-6950/'
     end
+  end
 
-    it 'should record the fixture' do
-      Covetous::Profile::Career.get @my_profile.url
+  describe 'when getting a profile' do
+    it 'should have all the hero names associated with that profile' do
+      @my_profile.hero_names.must_equal ['corrodee', 'corrodeath', 'corrodea', 'corrodead', 'corroder']
     end
   end
 end
